dedf87c8f567c692d0d706b346e9f58ffc23c2fa
hyojining <102501038+hyojining@users.noreply.github.com>
2023-11-15 13:20:18 +0900

    emaillist08 #2

--- clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
+++ clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
@@ -1,42 +1,51 @@
 package com.poscodx.emaillist.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import com.poscodx.emaillist.dto.JsonResult;
-import com.poscodx.emaillist.repository.EmailllistRepository;
 import com.poscodx.emaillist.vo.EmaillistVo;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@SuppressWarnings("unchecked")
+@RequestMapping("/api")
 public class ApiController {
-	@Autowired
-	private EmailllistRepository emaillistRepository;
+	private final RestTemplate restTemplate;
 	
-	@GetMapping("/api")
-	public ResponseEntity<JsonResult> read(@RequestParam(value="kw", required=true, defaultValue="") String keyword) {
-		log.info("Request[GET /api]:");
+	public ApiController(@LoadBalanced RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@GetMapping
+	public ResponseEntity<?> read(@RequestParam(value="kw", required=true, defaultValue="") String keyword) {
+		log.info("Request[GET /api]:" + keyword);
+		
+		Map<String, Object> response = restTemplate.getForObject("http://service-emaillist/?kw=" + keyword, Map.class);
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(JsonResult.success(emaillistRepository.findAll(keyword)));
+				.body(response);
 	}
 	
 	@PostMapping("/api")
-	public ResponseEntity<JsonResult> create(@RequestBody EmaillistVo vo) {
+	public ResponseEntity<?> create(@RequestBody EmaillistVo vo) {
 		log.info("Request[POST /api]:" + vo);
 		
-		emaillistRepository.insert(vo);
-		
+		Map<String, Object> response = restTemplate.postForObject("http://service-emaillist/", vo, Map.class);
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(JsonResult.success(vo));
+				.body(response);
 	}
 }
